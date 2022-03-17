4b2210c6e4ba44b6c48ad9a7236502d7ea3c660b
Eduardo Shibukawa <eduardo.shibukawa@thoughtworks.com>
2022-03-17 11:03:36 -0300

    Creating project

---
+++ src/main/java/com/eduardoshibukawatw/com/sonarqubepoc/SonarqubePocApplication.java
@@ -0,0 +1,13 @@
+package com.eduardoshibukawatw.com.sonarqubepoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SonarqubePocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SonarqubePocApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eduardoshibukawatw/com/sonarqubepoc/SonarqubePocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eduardoshibukawatw.com.sonarqubepoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SonarqubePocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
