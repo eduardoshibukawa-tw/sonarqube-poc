955ba85b69172b0c08498e7fb10bb59f59109a4f
Eduardo Shibukawa <eduardo.shibukawa@thoughtworks.com>
2022-03-17 16:16:09 -0300

    Add a controller

---
+++ src/main/java/com/eduardoshibukawatw/com/sonarqubepoc/controller/GrettingController.java
@@ -0,0 +1,14 @@
+package com.eduardoshibukawatw.com.sonarqubepoc.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GrettingController {
+
+    @GetMapping("hello")
+    public String gretting() {
+        return "Hello!";
+    }
+}
